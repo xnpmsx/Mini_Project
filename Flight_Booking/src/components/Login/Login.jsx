d2ceb178cc69c72c3a9adeef93e7b7c9a77d6ebf
BigcBow <s6404062610481@email.kmutnb.ac.th>
2023-08-17 13:01:52 +0700

    start login page

--- Flight_Booking/src/components/Login/Login.jsx
+++ Flight_Booking/src/components/Login/Login.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-import Home from '../Home/Home'
-
+import './Login.css'
 const Login = () => {
   return (
     <>
-        <Home></Home>
-        <div>
-        <h1>Login</h1>
+        <div className='homelog'>
+          <h1>Login</h1>
+          <div className="boxlog">
+            <form action="" method="post">
+              <label htmlFor="">Username : <input type="text" /></label><br />
+              <label htmlFor="">Password : <input type="text" /></label><br />
+              <label htmlFor="">Forgot Password</label>
+            </form>
+          </div>
+          
         </div>
     
     </>
