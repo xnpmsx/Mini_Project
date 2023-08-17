9828aa1186069dbe60f2b8e2c1b6817fd980665b
BigcBow <s6404062610481@email.kmutnb.ac.th>
2023-08-17 10:54:55 +0700

    remove navbar

--- Flight_Booking/src/App.jsx
+++ Flight_Booking/src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import Navbar from './components/Navbar/Navbar'
 import { Routes, Route, redirect } from 'react-router-dom'
 import Home from './components/Home/Home'
 import Login from './components/Login/Login'
--- Flight_Booking/src/components/Home/Home.jsx
+++ Flight_Booking/src/components/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Navbar from '../Navbar/Navbar'
 import './Home.css'
 import { Link } from 'react-router-dom'
 const Home = () => {
