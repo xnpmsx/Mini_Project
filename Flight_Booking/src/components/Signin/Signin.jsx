4ea361b88f6519f30317be2c7fb4175872ec7c32
BigcBow <s6404062610481@email.kmutnb.ac.th>
2023-08-20 18:05:37 +0700

    push css signin page

--- Flight_Booking/src/components/Signin/Signin.jsx
+++ Flight_Booking/src/components/Signin/Signin.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
-import Home from '../Home/Home'
+import './Signin.css'
 
 const Signin = () => {
   return (
     <>
-    <Home/>
+    <div class="wrapper">
+        <form action="">
+            <h1>Register</h1>
+            <div class="input-box">
+            </div>
+            <div class="input-box">
+
+            </div>
+            <div class="input-box">
+
+                
+            </div>
+            <button type="submit" class="btn">Register</button>
+            <div class="register-link">
+                <p>Already registered?
+                    <a href="loginpage.html">Login</a>
+                </p>
+
+            </div>
+        </form>
+    </div>
     </>
   )
 }
