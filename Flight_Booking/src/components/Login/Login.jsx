a5ef6322ab2a597aca8588831dec652b856f8850
BigcBow <s6404062610481@email.kmutnb.ac.th>
2023-08-20 18:03:21 +0700

    Make new login page

--- Flight_Booking/src/components/Login/Login.jsx
+++ Flight_Booking/src/components/Login/Login.jsx
@@ -1,19 +1,34 @@
 import React from 'react'
 import './Login.css'
+import { Link } from 'react-router-dom'
+import { FaUser } from 'react-icons/fa'
+import { HiLockClosed } from 'react-icons/hi'
 const Login = () => {
   return (
     <>
-        <div className='homelog'>
-          <h1>Login</h1>
-          <div className="boxlog">
-            <form action="" method="post">
-              <label htmlFor="">Username : <input type="text" /></label><br />
-              <label htmlFor="">Password : <input type="text" /></label><br />
-              <label htmlFor="">Forgot Password</label>
-            </form>
-          </div>
-          
-        </div>
+    <div className="container">
+    <div class="wrapper">
+        <form action="">
+            <h1>Login</h1>
+            <div class="input-box">
+              <input type="text" placeholder='Username' required  />
+              <div className="i"><FaUser /></div>
+            </div>
+            <div class="input-box">
+              <input type="password" placeholder='Password' required />
+              <div className="i"><HiLockClosed /></div>
+            </div>
+            <button type="submit" class="btn">Login</button>
+            <div class="register-link">
+                <p>Don't have an account ?
+                    <Link to="/Signin"> Register</Link>
+                </p>
+
+            </div>
+        </form>
+    </div>
+    </div>
+     
     
     </>
     
