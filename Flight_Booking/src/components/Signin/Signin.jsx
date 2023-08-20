820b232c79cd5343d70bcefb0d84adc5ca9a0640
BigcBow <s6404062610481@email.kmutnb.ac.th>
2023-08-20 18:10:18 +0700

    Add Icon

--- Flight_Booking/src/components/Signin/Signin.jsx
+++ Flight_Booking/src/components/Signin/Signin.jsx
@@ -1,29 +1,36 @@
 import React from 'react'
 import './Signin.css'
-
+import { FaUser } from 'react-icons/fa'
+import { HiLockClosed } from 'react-icons/hi'
+import { MdEmail } from 'react-icons/md'
 const Signin = () => {
   return (
     <>
-    <div class="wrapper">
-        <form action="">
-            <h1>Register</h1>
-            <div class="input-box">
-              <input type="text" placeholder='Username' required />
-            </div>
-            <div class="input-box">
-                <input type="email" name="" id="" placeholder='E-mail' />
-            </div>
-            <div class="input-box">
-                <input type="password" placeholder='Password' required />
-            </div>
-            <button type="submit" class="btn">Register</button>
-            <div class="register-link">
-                <p>Already registered?
-                    <a href="loginpage.html">Login</a>
-                </p>
+    <div className="container">
+      <div class="wrapper">
+          <form action="">
+              <h1>Register</h1>
+              <div class="input-box">
+                <input type="text" placeholder='Username' required />
+                <div className="i"><FaUser /></div>
+              </div>
+              <div class="input-box">
+                  <input type="email" name="" id="" placeholder='E-mail' />
+                  <div className="i"><MdEmail /></div>
+              </div>
+              <div class="input-box">
+                  <input type="password" placeholder='Password' required />
+                  <div className="i"><HiLockClosed /></div>
+              </div>
+              <button type="submit" class="btn">Register</button>
+              <div class="register-link">
+                  <p>Already registered?
+                      <a href="loginpage.html">Login</a>
+                  </p>
 
-            </div>
-        </form>
+              </div>
+          </form>
+      </div>
     </div>
     </>
   )
