b9333a2175cf8c2c5e11f47cefbf4475024eb3cb
BigcBow <s6404062610481@email.kmutnb.ac.th>
2023-09-26 17:23:48 +0700

    new register page

--- Flight_Booking/src/components/Signin/Signin.jsx
+++ Flight_Booking/src/components/Signin/Signin.jsx
@@ -7,40 +7,14 @@ import { Link } from 'react-router-dom'
 const Signin = () => {
   return (
     <>
-    <div className="container">
-      <div class="wrapper">
-          <form action="">
-              <h1>Register</h1>
-              <div class="input-box">
-                <input type="text" placeholder='Username' required />
-                <div className="i"><FaUser /></div>
-              </div>
-              <div class="input-box">
-                <input type="text" placeholder='Name' required />
-                <div className="i"><FaUser /></div>
-              </div>
-              <div class="input-box">
-                <input type="text" placeholder='Surname' required />
-                <div className="i"><FaUser /></div>
-              </div>
-              <div class="input-box">
-                  <input type="email" name="" id="" placeholder='E-mail' />
-                  <div className="i"><MdEmail /></div>
-              </div>
-              <div class="input-box">
-                  <input type="password" placeholder='Password' required />
-                  <div className="i"><HiLockClosed /></div>
-              </div>
-              <button type="submit" class="btn">Register</button>
-              <div class="register-link">
-                  <p>Already registered?
-                      <Link to="/Login"> Login</Link>
-                  </p>
-
-              </div>
-          </form>
+      <div className="bg">
+        <div className="img">
+            img
+        </div>
+        <div className="regis">
+            register
+        </div>
       </div>
-    </div>
     </>
   )
 }
