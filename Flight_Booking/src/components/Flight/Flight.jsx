0cac729308443ea6027cd95ca4672bf5cd7b5722
BigcBow <s6404062610481@email.kmutnb.ac.th>
2023-10-07 16:22:14 +0700

    add seat finish

--- Flight_Booking/src/components/Flight/Flight.jsx
+++ Flight_Booking/src/components/Flight/Flight.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Flight.css'
-import { FaCouch } from 'react-icons/fa'
 import { MdEventSeat } from 'react-icons/md'
 import { Link } from 'react-router-dom'
 const Flight = () => {
@@ -33,8 +32,6 @@ const Flight = () => {
       </div>
 
         <div className="seat_main_1">
-
-
             {/* red */}
             <div className="seat_1">
                 <div className="seat_left_1">
@@ -84,6 +81,8 @@ const Flight = () => {
                 
             </div>
 
+            {/* red */}
+
             <div className="seat_3">
                 <div className="seat_right_1">
                 <MdEventSeat color='red' size={'25'}/>
@@ -104,11 +103,233 @@ const Flight = () => {
                 <div className="seat_right_5">
                 <MdEventSeat color='red' size={'25'}/>
                 </div>
+            </div>
+        </div>
+
+
+        <div className="seat_main_2">
+            {/* red */}
+            <div className="seat_1_main_2">
+                <div className="seat_left_1">
+                <MdEventSeat color='red' size={'25'}/> 
+                </div>
+                
+                <div className="seat_left_2">
+                <MdEventSeat color='red' size={'25'}/>
+                </div>
+
+                <div className="seat_left_3">
+                <MdEventSeat color='red' size={'25'}/> 
+                </div>
+
+                <div className="seat_left_4">
+                <MdEventSeat color='red' size={'25'}/>
+                </div>
+
+                <div className="seat_left_5">
+                <MdEventSeat color='red' size={'25'}/> 
+                </div>
+              
+            </div>
+
+            {/* blue */}
+
+            <div className="seat_2_main_2">
+                <div className="seat_mid_1">
+                <MdEventSeat color='blue' size={'25'}/>
+                </div>
+
+                <div className="seat_mid_2">
+                <MdEventSeat color='blue' size={'25'}/>
+                </div>
+
+                <div className="seat_mid_3">
+                <MdEventSeat color='blue' size={'25'}/>
+                </div>
+
+                <div className="seat_mid_4">
+                <MdEventSeat color='blue' size={'25'}/>
+                </div>
+
+                <div className="seat_mid_5">
+                <MdEventSeat color='blue' size={'25'}/>
+                </div>
                 
             </div>
 
+            {/* red */}
 
+            <div className="seat_3_main_2">
+                <div className="seat_right_1">
+                <MdEventSeat color='red' size={'25'}/>
+                </div>
+
+                <div className="seat_right_2">
+                <MdEventSeat color='red' size={'25'}/>
+                </div>
+
+                <div className="seat_right_3">
+                <MdEventSeat color='red' size={'25'}/>
+                </div>
+
+                <div className="seat_right_4">
+                <MdEventSeat color='red' size={'25'}/>
+                </div>
+              
+                <div className="seat_right_5">
+                <MdEventSeat color='red' size={'25'}/>
+                </div>
+            </div>
         </div>
+        <div className="seat_main_3">
+            {/* red */}
+            <div className="seat_1_main_3">
+                <div className="seat_left_1">
+                <MdEventSeat color='red' size={'25'}/> 
+                </div>
+                
+                <div className="seat_left_2">
+                <MdEventSeat color='red' size={'25'}/>
+                </div>
+
+                <div className="seat_left_3">
+                <MdEventSeat color='red' size={'25'}/> 
+                </div>
+
+                <div className="seat_left_4">
+                <MdEventSeat color='red' size={'25'}/>
+                </div>
+
+                <div className="seat_left_5">
+                <MdEventSeat color='red' size={'25'}/> 
+                </div>
+              
+            </div>
+
+            {/* blue */}
+
+            <div className="seat_2_main_3">
+                <div className="seat_mid_1">
+                <MdEventSeat color='blue' size={'25'}/>
+                </div>
+
+                <div className="seat_mid_2">
+                <MdEventSeat color='blue' size={'25'}/>
+                </div>
+
+                <div className="seat_mid_3">
+                <MdEventSeat color='blue' size={'25'}/>
+                </div>
+
+                <div className="seat_mid_4">
+                <MdEventSeat color='blue' size={'25'}/>
+                </div>
+
+                <div className="seat_mid_5">
+                <MdEventSeat color='blue' size={'25'}/>
+                </div>
+                
+            </div>
+
+            {/* red */}
+
+            <div className="seat_3_main_3">
+                <div className="seat_right_1">
+                <MdEventSeat color='red' size={'25'}/>
+                </div>
+
+                <div className="seat_right_2">
+                <MdEventSeat color='red' size={'25'}/>
+                </div>
+
+                <div className="seat_right_3">
+                <MdEventSeat color='red' size={'25'}/>
+                </div>
+
+                <div className="seat_right_4">
+                <MdEventSeat color='red' size={'25'}/>
+                </div>
+              
+                <div className="seat_right_5">
+                <MdEventSeat color='red' size={'25'}/>
+                </div>
+            </div>
+        </div>
+        <div className="seat_main_4">
+            {/* red */}
+            <div className="seat_1_main_4">
+                <div className="seat_left_1">
+                <MdEventSeat color='red' size={'25'}/> 
+                </div>
+                
+                <div className="seat_left_2">
+                <MdEventSeat color='red' size={'25'}/>
+                </div>
+
+                <div className="seat_left_3">
+                <MdEventSeat color='red' size={'25'}/> 
+                </div>
+
+                <div className="seat_left_4">
+                <MdEventSeat color='red' size={'25'}/>
+                </div>
+
+                <div className="seat_left_5">
+                <MdEventSeat color='red' size={'25'}/> 
+                </div>
+              
+            </div>
+
+            {/* blue */}
+
+            <div className="seat_2_main_4">
+                <div className="seat_mid_1">
+                <MdEventSeat color='blue' size={'25'}/>
+                </div>
+
+                <div className="seat_mid_2">
+                <MdEventSeat color='blue' size={'25'}/>
+                </div>
+
+                <div className="seat_mid_3">
+                <MdEventSeat color='blue' size={'25'}/>
+                </div>
+
+                <div className="seat_mid_4">
+                <MdEventSeat color='blue' size={'25'}/>
+                </div>
+
+                <div className="seat_mid_5">
+                <MdEventSeat color='blue' size={'25'}/>
+                </div>
+                
+            </div>
+
+            {/* red */}
+
+            <div className="seat_3_main_4">
+                <div className="seat_right_1">
+                <MdEventSeat color='red' size={'25'}/>
+                </div>
+
+                <div className="seat_right_2">
+                <MdEventSeat color='red' size={'25'}/>
+                </div>
+
+                <div className="seat_right_3">
+                <MdEventSeat color='red' size={'25'}/>
+                </div>
+
+                <div className="seat_right_4">
+                <MdEventSeat color='red' size={'25'}/>
+                </div>
+              
+                <div className="seat_right_5">
+                <MdEventSeat color='red' size={'25'}/>
+                </div>
+            </div>
+        </div>
+
     </div>
     </>
   )
