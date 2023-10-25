a4406d533a0759b71b5335d5191446e7221ac8b5
BigcBow <s6404062610481@email.kmutnb.ac.th>
2023-10-25 17:55:37 +0700

    add popup for add admin

--- Flight_Booking/src/components/Admin/Admin.jsx
+++ Flight_Booking/src/components/Admin/Admin.jsx
@@ -1,109 +1,129 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { IoExitOutline } from 'react-icons/io5'
 import './Admin.css'
 
 function Admin() {
-  return (
-    <div>
-        <div className='admin-nav'>
-            <div className="admin-head">
-                <div className="canfly">
-                    <Link to='/admin'>Canfly</Link>
-                </div>
-                <div className="admin-topic">
-                    <div className="admin-topic-user">
-                        <Link to='/admin_user'>User</Link>
-                    </div>
-                    <div className="admin-topic-flight">
-                        <Link to='/admin_flight'>Flight</Link>
-                    </div>
-                </div>
-                <div className="admin">
-                    Admin name
-                </div>
-            </div>
-            <IoExitOutline className='icon-admin-exit' size={25} />
-        </div>
-        <div className="admin-main">
-            <div className="admin-main-user">
-                User : 10
-            </div>
-            <div className="admin-main-flight">
-                Flight : 15
-            </div>
-        </div>
-        <section>
-            <div className="list-user">
-                List Admin
-            </div>
 
-            <div className="admin-datauser">
-                <div className="admin-username">
-                    Username : TAGOYAGI
+    const [modal, setModal] = useState(false);
+
+    const toggleModal = () => {
+        setModal(!modal)
+    }
+
+    return (
+        <div>
+            <div className='admin-nav'>
+                <div className="admin-head">
+                    <div className="canfly">
+                        <Link to='/admin'>Canfly</Link>
+                    </div>
+                    <div className="admin-topic">
+                        <div className="admin-topic-user">
+                            <Link to='/admin_user'>User</Link>
+                        </div>
+                        <div className="admin-topic-flight">
+                            <Link to='/admin_flight'>Flight</Link>
+                        </div>
+                    </div>
+                    <div className="admin">
+                        Admin name
+                    </div>
                 </div>
-                <div className="admin-name">
-                    Name : Eiei
+                <IoExitOutline className='icon-admin-exit' size={25} />
+            </div>
+            <div className="admin-main">
+                <div className="admin-main-user">
+                    User : 10
                 </div>
-                <div className="admin-surname">
-                    Surname : Gaga
-                </div>
-                <div className="admin-phone">
-                    Phone : 0123456789
-                </div>
-                <div className="admin-edit">
-                    Edit
-                </div>
-                <div className="admin-delete">
-                    Delete
+                <div className="admin-main-flight">
+                    Flight : 15
                 </div>
             </div>
-            <div className="admin-datauser">
-                <div className="admin-username">
-                    Username : TAGOYAGI
+            <section>
+                <div className="list-user">
+                    List Admin
                 </div>
-                <div className="admin-name">
-                    Name : Eiei
+
+                <div className="admin-datauser">
+                    <div className="admin-username">
+                        Username : TAGOYAGI
+                    </div>
+                    <div className="admin-name">
+                        Name : Eiei
+                    </div>
+                    <div className="admin-surname">
+                        Surname : Gaga
+                    </div>
+                    <div className="admin-phone">
+                        Phone : 0123456789
+                    </div>
+                    <div className="admin-edit">
+                        Edit
+                    </div>
+                    <div className="admin-delete">
+                        Delete
+                    </div>
                 </div>
-                <div className="admin-surname">
-                    Surname : Gaga
+                <div className="admin-datauser">
+                    <div className="admin-username">
+                        Username : TAGOYAGI
+                    </div>
+                    <div className="admin-name">
+                        Name : Eiei
+                    </div>
+                    <div className="admin-surname">
+                        Surname : Gaga
+                    </div>
+                    <div className="admin-phone">
+                        Phone : 0123456789
+                    </div>
+                    <div className="admin-edit">
+                        Edit
+                    </div>
+                    <div className="admin-delete">
+                        Delete
+                    </div>
                 </div>
-                <div className="admin-phone">
-                    Phone : 0123456789
+                <div className="admin-datauser">
+                    <div className="admin-username">
+                        Username : TAGOYAGI
+                    </div>
+                    <div className="admin-name">
+                        Name : Eiei
+                    </div>
+                    <div className="admin-surname">
+                        Surname : Gaga
+                    </div>
+                    <div className="admin-phone">
+                        Phone : 0123456789
+                    </div>
+                    <div className="admin-edit">
+                        Edit
+                    </div>
+                    <div className="admin-delete">
+                        Delete
+                    </div>
                 </div>
-                <div className="admin-edit">
-                    Edit
+                <div className="admin-add-admin" onClick={toggleModal}>
+                    Add Admin
                 </div>
-                <div className="admin-delete">
-                    Delete
+            </section>
+
+            {modal && (
+                <div className="modal">
+                    <div onClick={toggleModal} className="overlay"></div>
+                    <div className="modal-content">
+                        Username <input type="text" />
+                        Name <input type="text" />
+                        <button className='button-add-admin' onClick={toggleModal}>Close</button>
+                    </div>
                 </div>
-            </div>
-            <div className="admin-datauser">
-                <div className="admin-username">
-                    Username : TAGOYAGI
-                </div>
-                <div className="admin-name">
-                    Name : Eiei
-                </div>
-                <div className="admin-surname">
-                    Surname : Gaga
-                </div>
-                <div className="admin-phone">
-                    Phone : 0123456789
-                </div>
-                <div className="admin-edit">
-                    Edit
-                </div>
-                <div className="admin-delete">
-                    Delete
-                </div>
-            </div>
-            <div className="admin-add-admin">
-                Add admin
-            </div>
-        </section>
-    </div>
-  )
+            )}
+
+            
+        </div>
+    )
 }
 
 export default Admin
--- Flight_Booking/src/components/Admin_flight/Admin_flight.jsx
+++ Flight_Booking/src/components/Admin_flight/Admin_flight.jsx
@@ -1,9 +1,98 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { IoExitOutline } from 'react-icons/io5'
+import './Admin_flight.css'
 
 function Admin_flight() {
   return (
     <div>
-        
+        <div className='admin-nav'>
+            <div className="admin-head">
+                <div className="canfly">
+                    <Link to='/admin'>Canfly</Link>
+                </div>
+                <div className="admin-topic">
+                    <div className="admin-topic-user">
+                        <Link to='/admin_user'>User</Link>
+                    </div>
+                    <div className="admin-topic-flight">
+                        <Link to='/admin_flight'>Flight</Link>
+                    </div>
+                </div>
+                <div className="admin">
+                    Admin name
+                </div>
+            </div>
+            <IoExitOutline className='icon-admin-exit' size={25} />
+        </div>
+        <section>
+            <div className="list-user">
+                List Flight
+            </div>
+            <div className="admin-datauser">
+                <div className="admin-username">
+                    Airline : Nokair
+                </div>
+                <div className="admin-name">
+                    Destination : Puket
+                </div>
+                <div className="admin-surname">
+                    Date : 2566-12-01
+                </div>
+                <div className="admin-phone">
+                    Time : 09:30:00
+                </div>
+                <div className="admin-edit">
+                    Edit
+                </div>
+                <div className="admin-delete">
+                    Delete
+                </div>
+            </div>
+            <div className="admin-datauser">
+                <div className="admin-username">
+                    Airline : Nokair
+                </div>
+                <div className="admin-name">
+                    Destination : Puket
+                </div>
+                <div className="admin-surname">
+                    Date : 2566-12-01
+                </div>
+                <div className="admin-phone">
+                    Time : 09:30:00
+                </div>
+                <div className="admin-edit">
+                    Edit
+                </div>
+                <div className="admin-delete">
+                    Delete
+                </div>
+            </div>
+            <div className="admin-datauser">
+                <div className="admin-username">
+                    Airline : Nokair
+                </div>
+                <div className="admin-name">
+                    Destination : Puket
+                </div>
+                <div className="admin-surname">
+                    Date : 2566-12-01
+                </div>
+                <div className="admin-phone">
+                    Time : 09:30:00
+                </div>
+                <div className="admin-edit">
+                    Edit
+                </div>
+                <div className="admin-delete">
+                    Delete
+                </div>
+            </div>
+            <div className="admin-add-user">
+                Add Flight
+            </div>
+        </section>
     </div>
   )
 }
