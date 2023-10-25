dc44a4f37e99af53af950255c961248711799b90
BigcBow <s6404062610481@email.kmutnb.ac.th>
2023-10-25 19:13:10 +0700

    add edit popup on admin_user page

--- Flight_Booking/src/components/Admin_user/Admin_user.jsx
+++ Flight_Booking/src/components/Admin_user/Admin_user.jsx
@@ -10,9 +10,14 @@ function exit(){
 function Admin_user() {
 
     const [modal, setModal] = useState(false);
+    const [edit, setEdit] = useState(false);
 
     const toggleModal = () => {
         setModal(!modal)
+    }
+
+    const toggleEdit = () => {
+        setEdit(!edit)
     }
 
   return (
@@ -53,7 +58,7 @@ function Admin_user() {
                 <div className="admin-phone">
                     Phone : 0123456789
                 </div>
-                <div className="admin-edit">
+                <div className="admin-edit" onClick={toggleEdit}>
                     Edit
                 </div>
                 <div className="admin-delete">
@@ -73,7 +78,7 @@ function Admin_user() {
                 <div className="admin-phone">
                     Phone : 0123456789
                 </div>
-                <div className="admin-edit">
+                <div className="admin-edit" onClick={toggleEdit}>
                     Edit
                 </div>
                 <div className="admin-delete">
@@ -93,7 +98,7 @@ function Admin_user() {
                 <div className="admin-phone">
                     Phone : 0123456789
                 </div>
-                <div className="admin-edit">
+                <div className="admin-edit" onClick={toggleEdit}>
                     Edit
                 </div>
                 <div className="admin-delete">
@@ -105,7 +110,7 @@ function Admin_user() {
             </div>
         </section>
 
-        {modal && (
+            {modal && (
                 <div className="modal">
                     <div onClick={toggleModal} className="overlay"></div>
                     <div className="modal-content-user">
@@ -144,6 +149,47 @@ function Admin_user() {
                         </div>
                     </div>
                 </div>
+            )}
+
+            {edit && (
+                <div className="modal">
+                <div onClick={toggleEdit} className="overlay"></div>
+                <div className="modal-content-user">
+
+                    <h2>Edit User</h2>
+
+                    <div className="modal-content-input-user">
+
+                        <div className="modal-content-input-user-1">
+                            <div className="modal-content-username">
+                                Username <input type="text" className='modal-input' />
+                            </div>
+                            <div className="modal-content-username">
+                                Password <input type="password" className='modal-input' />
+                            </div>
+                            <div className="modal-content-password">
+                                Name <input type="text" className='modal-input-name' />
+                            </div>
+                        </div>
+                        <div className="modal-content-input-user-2">
+                            <div className="modal-content-username">
+                                Surname <input type="text" className='modal-input' />
+                            </div>
+                            <div className="modal-content-username">
+                                Email <input type="email" className='modal-input-email' />
+                            </div>
+                            <div className="modal-content-password">
+                                Phone <input type="number" className='modal-input-phone' />
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div className="modal-button">
+                        <button className='button-edit-admin'>Edit</button>
+                        <button className='button-close-admin' onClick={toggleEdit}>Close</button>
+                    </div>
+                </div>
+            </div>
             )}
         
     </div>
